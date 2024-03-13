ede0185df8b5afb480debd8a3f539556a6881e6f
Zachary <zgarc025@fiu.edu>
2024-03-13 01:43:01 -0400

    add getRepos to GithubContext

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -35,6 +35,33 @@ const GithubState = (props) => {
     });
   };
 
+  const getUser = async (login) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${login}?client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data,
+    });
+  };
+
+  const getUserRepos = async (login) => {
+    const res = await axios.get(
+      `https://api.github.com/users/${login}/repos?per_page=5&sort=created:asc&?client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  };
+
   const clearUsers = () => {
     dispatch({ type: CLEAR_USERS });
   };
@@ -50,6 +77,8 @@ const GithubState = (props) => {
         loading: state.loading,
         searchUsers,
         clearUsers,
+        getUser,
+        getUserRepos,
       }}
     >
       {props.children}
